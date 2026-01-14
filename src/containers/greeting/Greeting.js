b6c77e197c9fb5b82b88aafbfc5a6dddb61c66d8
Ziyan <ziyan.offl@gmail.com>
2026-01-14 19:55:02 +0400

    enhancements

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -42,8 +42,8 @@ export default function Greeting() {
                 <Button text="Contact me" href="#contact" />
                 {greeting.resumeLink && (
                   <a
-                    href={require("./resume.pdf")}
-                    download="Resume.pdf"
+                    href={greeting.resumeLink}
+                    download="Ziyan-Rilwan.pdf"
                     className="download-link-button"
                   >
                     <Button text="Download my resume" />
